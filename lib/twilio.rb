c50a011809e367be1afb9c6ab70810986ddabbce
Ilana Sufrin <ilanasufrin@gmail.com>
2014-07-12 15:26:57 -0400

    added twilio stuff

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,14 +1,14 @@
 # Get twilio-ruby from twilio.com/docs/ruby/install
-         # This line not needed for ruby > 1.8
+      
 require 'twilio-ruby'
  
 # Get your Account Sid and Auth Token from twilio.com/user/account
-account_sid = 'AC5ef8732a3c49700934481addd5ce1659'
-auth_token = '{{ auth_token }}'
+account_sid = 'AC491f53e83618d21896f2ae3d1be302d8'
+auth_token = ENV['TWILIO_API_KEY']
 @client = Twilio::REST::Client.new account_sid, auth_token
  
-message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-    :to => "+15558675309",
-    :from => "+14158141829",
-    :media_url => "http://www.example.com/hearts.png")
+message = @client.account.messages.create(:body => "The free stuff fairy has found an item for you! Please visit #{ADD THIS URL} to accept or deny the offer." ,
+    :to => "+14125088025",
+    :from => "+14123075863",
+   # :media_url => "http://www.example.com/hearts.png")
 puts message.to
