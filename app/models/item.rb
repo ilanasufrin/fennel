b7a01addff158e20dbe59263b559e8c74e280c22
Jesse Kipp <esbe.v2@gmail.com>
2014-07-11 22:04:18 -0400

    Adds required validator for paperclip photo

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   has_attached_file :photo, styles: { large: "800x800>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
-  #validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   has_many :offers
 
